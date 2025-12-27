abb6999eacf176dc1783d120c36f999d949da0cc
Muhammad Fouad <mohammedd.foad@gmail.com>
2025-12-27 16:43:31 +0200

    Time: 0 ms (100.00%) | Memory: 8.9 MB (62.70%) - LeetSync

---
+++ LeetCode/3379-score-of-a-string/score-of-a-string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int scoreOfString(string &s) {
+        int sum = 0;
+        for (int i = 0; i < s.size() - 1; ++i) {
+            if (s[i] > s[i + 1])
+                sum += s[i] - s[i + 1];
+            else
+                sum += s[i + 1] - s[i];
+        }  
+        return sum;
+    }
+};
