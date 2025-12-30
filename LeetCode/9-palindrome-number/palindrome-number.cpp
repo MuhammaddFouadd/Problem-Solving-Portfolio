b0bbd2d8c51db41e8cf1705d63b8b6eb3b23dbd4
Muhammad Fouad <mohammedd.foad@gmail.com>
2025-12-30 18:34:48 +0200

    Time: 0 ms (100.00%) | Memory: 8.6 MB (40.79%) - LeetSync

--- LeetCode/9-palindrome-number/palindrome-number.cpp
+++ LeetCode/9-palindrome-number/palindrome-number.cpp
@@ -1,25 +1,17 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-
-        if (x < 0)
+        if(x < 0) {
             return false;
-
-        deque<int> deq;
-        while (x) {
-            deq.push_front(x % 10);
-            x /= 10;
         }
-
-        int i{}, y = deq.size() - 1;
-
-        while (i <= y) {
-            if (i == y)
-                return true;
-
-            if (deq[i++] != deq[y--])
-                return false;
+        
+        long long rev = 0,copy = x;
+        
+        while(copy != 0) {
+            rev = rev * 10 + copy % 10;
+            copy /= 10;
         }
-        return true;
+        
+        return rev == x;
     }
 };
