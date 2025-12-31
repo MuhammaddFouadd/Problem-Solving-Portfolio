d76002c91fd2edcc2304ad8da679396cb1794664
Muhammad Fouad <mohammedd.foad@gmail.com>
2025-12-31 22:27:36 +0200

    Time: 0 ms (100.00%) | Memory: 63 MB (11.71%) - LeetSync

--- LeetCode/11-container-with-most-water/container-with-most-water.cpp
+++ LeetCode/11-container-with-most-water/container-with-most-water.cpp
@@ -1,27 +1,19 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-        vector<pair<int, int>> v;
-        int len = height.size();
-        for (int i = 0; i < len; ++i) {
-            v.push_back({i, height[i]});
-        }
 
         int maxArea = -1;
-        int i = 0, j = len - 1;
+        int i = 0, j = height.size() - 1;
 
         while (i<j) {
-            int area =
-                min(v[i].second, v[j].second) *
-                (max(v[j].first, v[i].first) - min(v[j].first, v[i].first));
+            int area = min(height[i], height[j]) * (j-i);
             maxArea = max(maxArea, area);
 
-            if (v[i].second < v[j].second)
+            if (height[i] < height[j])
                 ++i;
             else
                 --j;
         }
-
         return maxArea;
     }
 };
