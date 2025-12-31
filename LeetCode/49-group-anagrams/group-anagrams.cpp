891374aa8537684d0c86b17891be42f1c4dd5399
Muhammad Fouad <mohammedd.foad@gmail.com>
2026-01-01 01:44:37 +0200

    Time: 14 ms (74.18%) | Memory: 24.9 MB (76.20%) - LeetSync

--- LeetCode/49-group-anagrams/group-anagrams.cpp
+++ LeetCode/49-group-anagrams/group-anagrams.cpp
@@ -1,22 +1,21 @@
 
 class Solution {
 public:
-    vector<vector<string>> groupAnagrams(vector<string> &strs)
-{
-    map<string, vector<string>> st;
-    int ln = strs.size();
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> st;
+        int ln = strs.size();
 
-    for (int i = 0; i < ln; ++i){
-        string k = strs[i];
-        sort(strs[i].begin(),strs[i].end());
-        st[strs[i]].push_back(k);
+        for (int i = 0; i < ln; ++i) {
+            string k = strs[i];
+            sort(strs[i].begin(), strs[i].end());
+            st[move(strs[i])].push_back(k);
+        }
+
+        vector<vector<string>> out;
+        for (auto it : st) {
+            out.push_back(move(it.second));
+        }
+
+        return out;
     }
-
-    vector <vector<string>> out ;
-    for(auto it :st){
-        out.push_back(it.second);
-    }
-
-    return out ;
-}
 };
