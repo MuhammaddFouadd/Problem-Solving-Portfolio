e2965794b51a8a6279a74369e57d6a86ad0eb2bc
Muhammad Fouad <mohammedd.foad@gmail.com>
2026-01-01 01:39:42 +0200

    Time: 28 ms (24.29%) | Memory: 26.2 MB (38.27%) - LeetSync

--- LeetCode/49-group-anagrams/group-anagrams.cpp
+++ LeetCode/49-group-anagrams/group-anagrams.cpp
@@ -2,54 +2,21 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string> &strs)
-    {
-        vector<pair<string, string>> s;
-        int ln = strs.size();
+{
+    map<string, vector<string>> st;
+    int ln = strs.size();
 
-        for (int i = 0; i < ln; ++i)
-        {
-            int sz = strs[i].size();
-            string second = strs[i];
-
-            sort(strs[i].begin(), strs[i].end());
-            string first = strs[i];
-
-            s.push_back({first, second});
-        }
-
-        sort(s.begin(), s.end());
-
-        vector<pair<string, string>> v;
-
-        for (auto it : s)
-        {
-            v.push_back(it);
-        }
-
-        vector<vector<string>> out;
-
-        int i = 0, j = 1;
-        while ((i < j && i < ln) || i == 0)
-        {
-            vector<string> temp;
-            temp.push_back(v[i].second);
-
-            if (j >= ln)
-            {
-                out.push_back(temp);
-                break;
-            }
-
-            while (j < ln && v[i].first == v[j].first)
-            {
-                temp.push_back(v[j].second);
-                ++j;
-            }
-            out.push_back(temp);
-            i = j;
-            j++;
-        }
-
-        return out;
+    for (int i = 0; i < ln; ++i){
+        string k = strs[i];
+        sort(strs[i].begin(),strs[i].end());
+        st[strs[i]].push_back(k);
     }
+
+    vector <vector<string>> out ;
+    for(auto it :st){
+        out.push_back(it.second);
+    }
+
+    return out ;
+}
 };
