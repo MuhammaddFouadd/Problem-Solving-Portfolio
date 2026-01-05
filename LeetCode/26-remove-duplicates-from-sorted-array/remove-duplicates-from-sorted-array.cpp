22724def6b416a471386d726a9bc43b5d667709e
Muhammad Fouad <mohammedd.foad@gmail.com>
2026-01-05 21:23:52 +0200

    Time: 0 ms (100.00%) | Memory: 23.6 MB (6.41%) - LeetSync

---
+++ LeetCode/26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        set <int>s;
+
+        for (auto it : nums){
+            s.insert(it);
+        }
+
+        int i = 0;
+        for(auto it : s){
+            nums[i++]=it;
+        }
+
+        return i;
+    }
+};
