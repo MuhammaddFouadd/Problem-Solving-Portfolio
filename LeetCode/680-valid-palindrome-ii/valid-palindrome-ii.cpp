b29da31de7744a9fbaa5c152dc8a4f621311bba8
Muhammad Fouad <mohammedd.foad@gmail.com>
2026-01-06 23:03:29 +0200

    Time: 3 ms (70.73%) | Memory: 26.5 MB (46.44%) - LeetSync

---
+++ LeetCode/680-valid-palindrome-ii/valid-palindrome-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool validPalindrome(string s, int i, int j) {
+        while (i < j) {
+            if (s[i++] != s[j--]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    bool validPalindrome(string s) {
+
+        int i = 0, j = s.size() - 1;
+        while (i < j) {
+            if (s[i] != s[j]) {
+                return validPalindrome(s, i, j - 1) ||
+                       validPalindrome(s, i + 1, j);
+            }
+            ++i, --j;
+        }
+
+        return true;
+    }
+};
