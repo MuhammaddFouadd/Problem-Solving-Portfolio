4ccb8bf0d08cbef986245a7bf22e2e9697c2030b
Muhammad Fouad <mohammedd.foad@gmail.com>
2026-01-07 14:04:04 +0200

    Time: 1 ms (2.20%) | Memory: 9.4 MB (54.92%) - LeetSync

---
+++ LeetCode/225-implement-stack-using-queues/implement-stack-using-queues.cpp
@@ -0,0 +1,32 @@
+class MyStack {
+public:
+    deque<int> stack;
+    MyStack() { }
+
+    void push(int x) {
+        stack.push_back(x);
+    }
+
+    int pop() {
+        int top = stack.back();
+        stack.pop_back();
+        return top;
+    }
+
+    int top() {
+        return stack.back();
+    }
+
+    bool empty() {
+        return stack.empty();
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
