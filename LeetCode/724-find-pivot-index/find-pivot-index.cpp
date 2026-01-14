9dc037f08ee5ef26f64e4fe7a552ddc6b74da109
Muhammad Fouad <mohammedd.foad@gmail.com>
2026-01-14 23:52:49 +0200

    Time: 0 ms (100.00%) | Memory: 35.5 MB (95.67%) - LeetSync

---
+++ LeetCode/724-find-pivot-index/find-pivot-index.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int sumPre{}, sumSu{};
+        for (int i = 0; i < nums.size(); ++i) {
+            sumSu += nums[i];
+        }
+
+        for (int i = 0; i < nums.size(); ++i) {
+            if (i != 0)
+                sumPre += nums[i - 1];
+
+            sumSu -= nums[i];
+            if (sumPre == sumSu)
+                return i;
+        }
+
+        return -1;
+    }
+};
