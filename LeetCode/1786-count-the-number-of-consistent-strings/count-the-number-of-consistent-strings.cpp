3c826ba94660ed66bb98e08044b67993122b40b4
Muhammad Fouad <mohammedd.foad@gmail.com>
2026-01-15 22:03:32 +0200

    Time: 0 ms (100.00%) | Memory: 34.2 MB (20.00%) - LeetSync

--- LeetCode/1786-count-the-number-of-consistent-strings/count-the-number-of-consistent-strings.cpp
+++ LeetCode/1786-count-the-number-of-consistent-strings/count-the-number-of-consistent-strings.cpp
@@ -1,11 +1,16 @@
 class Solution {
 public:
     int countConsistentStrings(string allowed, vector<string>& words) {
+        unordered_map<char, bool> m;
+        for (int i = 0; i < allowed.size(); ++i) {
+            m[allowed[i]] = 1;
+        }
+
         int cnt {};
         for (int i = 0; i < words.size(); ++i) {
             bool consis = 1;
             for (int j = 0; j < words[i].size(); ++j) {
-                if (allowed.find(words[i][j]) == string::npos)
+                if (m.find(words[i][j]) == m.end())
                 {
                     consis =0;
                     break;
