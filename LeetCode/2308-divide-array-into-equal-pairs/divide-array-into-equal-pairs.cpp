e38b1e305c84f90b347e655bdd6b72915b8ce849
Muhammad Fouad <mohammedd.foad@gmail.com>
2026-01-17 20:24:51 +0200

    Time: 5 ms (31.27%) | Memory: 18.3 MB (27.73%) - LeetSync

---
+++ LeetCode/2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool divideArray(vector<int>& nums) {
+        unordered_map<int, int> freq;
+
+        for (int i = 0; i < nums.size(); ++i) {
+            if (freq.find(nums[i]) == freq.end()) {
+                freq[nums[i]] = 1;
+                continue;
+            }
+            ++freq[nums[i]];
+        }
+
+        for (int i = 0; i < nums.size(); ++i) {
+
+            if (freq[nums[i]] % 2 != 0)
+                return false;
+        }
+
+        return true;
+    }
+};
