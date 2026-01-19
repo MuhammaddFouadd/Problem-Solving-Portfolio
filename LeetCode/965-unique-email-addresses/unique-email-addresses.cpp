a79be146e982489db76cfcf8729b552554b0a30c
Muhammad Fouad <mohammedd.foad@gmail.com>
2026-01-19 15:03:18 +0200

    Time: 7 ms (61.81%) | Memory: 17.4 MB (81.09%) - LeetSync

---
+++ LeetCode/965-unique-email-addresses/unique-email-addresses.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int numUniqueEmails(vector<string>& emails) {
+        unordered_map<string, bool> check;
+
+        for (int i = 0; i < emails.size(); ++i) {
+            int len = emails[i].find('@');
+            string local{};
+            for (int j = 0; j < len; ++j) {
+                if (emails[i][j] == '.')
+                    continue;
+                else if (emails[i][j] == '+')
+                    break;
+                local += emails[i][j];
+            }
+
+            for (int j = len; j < emails[i].size(); ++j) {
+                local += emails[i][j];
+            }
+
+            if (check.find(local) == check.end())
+                check[local] = 1;
+        }
+
+        return check.size();
+    }
+};
