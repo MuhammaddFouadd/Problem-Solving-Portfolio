b7150ff84f29db290d87a15cfa5f9f842cf4f07d
Muhammad Fouad <mohammedd.foad@gmail.com>
2026-02-08 07:57:16 +0200

    Time: 3022 ms (5.02%) | Memory: 447 MB (5.83%) - LeetSync

---
+++ LeetCode/15-3sum/3sum.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> v;
+
+        for (int i = 0; i < nums.size(); ++i) {
+            int st = i+1, en = nums.size() - 1;
+            while (st < en) {
+                if (st == i) {
+                    ++st;
+                    continue;
+                } else if (en == i) {
+                    --en;
+                    continue;
+                }
+
+                if (nums[i] + nums[st] + nums[en] > 0) {
+                    --en;
+                } else if (nums[i] + nums[st] + nums[en] < 0) {
+                    ++st;
+                } else {
+                    v.push_back({nums[i], nums[st], nums[en]});
+                    ++st, --en;
+                }
+            }
+        }
+
+        for (int i = 0; i < v.size(); ++i) {
+            sort(v[i].begin(), v[i].end());
+        }
+        sort(v.begin(), v.end());
+        auto last = unique(v.begin(), v.end());
+        v.erase(last, v.end());
+
+        return v;
+    }
+};
